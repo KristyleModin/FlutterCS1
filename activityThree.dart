5775f5d805f247408acf852edfc40c4d0f49ea32
KristyleModin <155639837+KristyleModin@users.noreply.github.com>
2025-03-06 14:30:37 +0800

    Update and rename activityfour.dart to activityThree.dart

---
+++ activityThree.dart
@@ -0,0 +1,180 @@
+import 'package:flutter/material.dart';
+
+class Practice extends StatelessWidget {
+  const Practice({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // Get screen size
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
+    final isMobile = screenWidth < 600; // Adjust layout for mobile screens
+
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: Image.network(
+              'https://images.pexels.com/photos/1097930/pexels-photo-1097930.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+              fit: BoxFit.cover,
+            ),
+          ),
+          Align(
+            alignment: Alignment.center,
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: isMobile ? 20 : screenWidth * 0.2, // Adjust padding for smaller screens
+                  vertical: isMobile ? 50 : 150,
+                ),
+                child: Container(
+                  width: isMobile ? screenWidth * 0.9 : 400, // Adjust width dynamically
+                  padding: const EdgeInsets.all(25),
+                  decoration: BoxDecoration(
+                    color: Colors.transparent,
+                  ),
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        width: double.infinity,
+                        child: Text(
+                          "5 - Modin",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: isMobile ? 28 : 35,
+                            color: Colors.blueAccent,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      SizedBox(
+                        width: double.infinity,
+                        child: Text(
+                          'Login',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: isMobile ? 28 : 35,
+                            color: Colors.blueGrey,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      SizedBox(
+                        width: double.infinity,
+                        child: Text(
+                          "Don't have an account? Create your account, \n it takes less than a minute",
+                          style: TextStyle(
+                            fontSize: isMobile ? 12 : 14,
+                            color: Colors.blueGrey[700],
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      TextField(
+                        decoration: InputDecoration(
+                          prefixIcon: Icon(Icons.person),
+                          hintText: "youremail@company.com",
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      TextField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          prefixIcon: Icon(Icons.lock),
+                          suffixIcon: Icon(Icons.visibility_off),
+                          hintText: "Password",
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                            children: [
+                              Checkbox(value: false, onChanged: (value) {}),
+                              Text("Remember me"),
+                            ],
+                          ),
+                          TextButton(
+                            onPressed: () {},
+                            child: Text('Forgot Password'),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 20),
+                      SizedBox(
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.blue,
+                            padding: EdgeInsets.symmetric(vertical: 15),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
+                          child: Text(
+                            "Login",
+                            style: TextStyle(
+                              fontSize: 18,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      Text(
+                        'Login with',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: isMobile ? 12 : 14,
+                          color: Colors.blueGrey,
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          IconButton(
+                            icon: Icon(Icons.facebook, color: Colors.blue),
+                            onPressed: () {},
+                          ),
+                          IconButton(
+                            icon: Icon(Icons.favorite, color: Colors.deepPurple[400]),
+                            onPressed: () {},
+                          ),
+                          IconButton(
+                            icon: Icon(Icons.g_mobiledata, color: Colors.red),
+                            onPressed: () {},
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 20),
+                      Text(
+                        "@ Copyright 2019 - Drivester Ltd. \n 67 Albion Street, Wesk Yorkshire, Leeds LS1 5AA, United Kingdom",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: isMobile ? 10 : 12,
+                          color: Colors.blueGrey,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
